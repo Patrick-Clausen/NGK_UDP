e7f63dfa84eb42c785b707dfe4d03fb7266996c8
Patrick-Clausen <60315389+Patrick-Clausen@users.noreply.github.com>
2020-10-16 11:15:15 +0200

    Started Exercise, made server

---
+++ UDP_Exercise/measurement_server/Server.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace measurement_server
+{
+    public struct UDPMessage
+    {
+        public string Message;
+        public IPEndPoint Address;
+    }
+
+    public class Server
+    {
+        private UdpClient _socket;
+
+        public Server(int port)
+        {
+            _socket = new UdpClient(new IPEndPoint(IPAddress.Any, 9000));
+        }
+
+        public void Send(UDPMessage message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message.Message);
+            _socket.Send(data, data.Length, message.Address);
+        }
+
+        public async Task<UDPMessage> Receive()
+        {
+            var result = await _socket.ReceiveAsync();
+            return new UDPMessage()
+            {
+                Message = Encoding.UTF8.GetString(result.Buffer, 0, result.Buffer.Length),
+                Address = result.RemoteEndPoint
+            };
+        }
+    }
+}
